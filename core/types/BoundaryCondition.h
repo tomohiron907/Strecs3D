fe4ab9e39e76de63156319e27bdc5f879217f6e2
Taniguchi Tomohiro <124371774+tomohiron907@users.noreply.github.com>
2025-11-10 16:12:10 +0900

    add boundary condition type

---
+++ core/types/BoundaryCondition.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+// 3次元ベクトル構造体
+struct Vector3D {
+    double x;
+    double y;
+    double z;
+};
+
+// 拘束条件構造体
+struct ConstrainCondition {
+    int surface_id;
+    std::string name;
+};
+
+// 荷重条件構造体
+struct LoadCondition {
+    int surface_id;
+    std::string name;
+    double magnitude;
+    Vector3D direction;
+};
+
+// 境界条件構造体
+struct BoundaryCondition {
+    std::vector<ConstrainCondition> constrains;
+    std::vector<LoadCondition> loads;
+};
