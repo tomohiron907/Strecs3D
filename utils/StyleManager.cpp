a829b221b2d72a24c805c8a3f3b68b1bd079f064
Taniguchi Tomohiro <taniguchitomohiro@TaniguchinoMacBook-Pro.local>
2026-01-25 23:37:04 +0900

    update button style

--- utils/StyleManager.cpp
+++ utils/StyleManager.cpp
@@ -13,15 +13,13 @@ const int StyleManager::MARGIN_SMALL = 4;
 const int StyleManager::MARGIN_MEDIUM = 8;
 const int StyleManager::MARGIN_LARGE = 16;
 
-// Button Style (n = 4px)
-const int StyleManager::BUTTON_RADIUS = StyleManager::RADIUS_SMALL;
-const int StyleManager::BUTTON_MARGIN = StyleManager::MARGIN_SMALL;
+// Button Style 
+const int StyleManager::BUTTON_RADIUS = StyleManager::RADIUS_MEDIUM;
+const int StyleManager::BUTTON_MARGIN = StyleManager::MARGIN_MEDIUM;
 
 // Container Style
-// n px (Button Radius) の角丸要素に対して、余白 m px (Container Padding) を追加するとき、
-// コンテナ要素は n + m px の角丸にする
-const int StyleManager::CONTAINER_PADDING = StyleManager::MARGIN_MEDIUM; // m = 8px
-const int StyleManager::CONTAINER_RADIUS = StyleManager::BUTTON_RADIUS + StyleManager::CONTAINER_PADDING; // 4 + 8 = 12px
+const int StyleManager::CONTAINER_PADDING = StyleManager::MARGIN_MEDIUM;
+const int StyleManager::CONTAINER_RADIUS = StyleManager::BUTTON_RADIUS + StyleManager::CONTAINER_PADDING;
 
 // Input Field Style
 const int StyleManager::INPUT_HEIGHT = 28;
